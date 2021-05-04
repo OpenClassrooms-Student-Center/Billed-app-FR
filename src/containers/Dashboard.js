ace0e4a7c37dd45399b3f9137f3324001b765238
william <alown666@gmail.com>
2021-05-04 08:49:14 +0200

    :hammer: fix : Correction du bug qui dupliquais des events sur les factures

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -145,7 +145,7 @@ export default class {
       this.counter ++
     }
 
-    bills.forEach(bill => {
+    filteredBills(bills, getStatus(this.index)).forEach(bill => {
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
