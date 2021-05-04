0f1947b2120fff358c15a74aff0b70e3a1e7f78c
william <alown666@gmail.com>
2021-05-04 08:35:25 +0200

    :hammer; fix : Math.min sur la taille des images affiché

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -80,8 +80,7 @@ export default class {
 
   handleClickIconEye = () => {
     const billUrl = $('#icon-eye-d').attr("data-bill-url")
-    let modalAdminWidth = $('#modaleFileAdmin1').width() > 800 ? 800 : $('#modaleFileAdmin1').width()
-    const imgWidth = Math.floor(modalAdminWidth* 0.8)
+    const imgWidth = Math.floor(Math.min($('#modaleFileAdmin1').width(), 800)* 0.8)
     $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
