47e0a7679ab41d9a6248b440bfcecc45ea08424b
william <alown666@gmail.com>
2021-05-04 08:26:26 +0200

    :hammer: fix : AjoutDe math.min pour la taille de la preview image

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -22,7 +22,7 @@ export default class {
 
   handleClickIconEye = (icon) => {
     const billUrl = icon.getAttribute("data-bill-url")
-    const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
+    const imgWidth = Math.floor(Math.min($('#modaleFile').width(), 800) * 0.5)
     $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
     $('#modaleFile').modal('show')
   }
