442a441b8fd14eadd30d878d1a84cfbbea6a06de
Louis Da Costa <louis.da-costa@expleogroup.com>
2021-07-05 23:43:19 +0200

    Add feature for the upload of a file

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -55,7 +55,7 @@ export default () => {
                   </div>
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
-                    <input required type="file" class="form-control blue-border" data-testid="file" />
+                    <input required type="file" class="form-control blue-border" accept="image/*" data-testid="file" />
                   </div>
                 </div>
             </div>
