5b48a62b08d18bee58ffdd81cc37d72deb5bd96b
LouisMaupas <louismaupas@gmail.com>
2021-09-20 11:20:12 +0200

    BUG2 : [Bug report] - Login fixed

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -32,11 +32,12 @@ export default class Login {
     this.document.body.style.backgroundColor="#fff"
   }
 
+  // FIXED [Bug report] - Login employee => admin
   handleSubmitAdmin = e => {
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
