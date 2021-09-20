49987652b68784c30e518be296e7cdac8ee05499
LouisMaupas <louismaupas@gmail.com>
2021-09-20 16:49:36 +0200

    commented

--- src/app/format.js
+++ src/app/format.js
@@ -6,6 +6,15 @@ export const formatDate = (dateStr) => {
   const da = new Intl.DateTimeFormat('fr', { day: '2-digit' })
   const month = mo.toString().charAt(0).toUpperCase() + mo.toString().slice(1)
   return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+
+  // Before "fix"
+  /*
+  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
+  const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
+  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+  const month = mo.charAt(0).toUpperCase() + mo.slice(1)
+  return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+  */
 }
  
 export const formatStatus = (status) => {
